a29ed3e8cd5b1e5647ca3d81b7af0a73a085757d
Kokchun <kokchungiang@gmail.com>
2024-02-22 10:31:39 +0100

    gapmidner in py script

---
+++ code_alongs/L8_1_gapminder.py
@@ -0,0 +1,20 @@
+import plotly_express as px 
+
+gapminder = px.data.gapminder()
+
+fig = px.scatter(
+    gapminder,
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="country",
+    size_max=70,
+    log_x=True,
+    animation_frame="year",
+    animation_group="country", 
+    title="Gapminder",
+    range_y=[25,90],
+    range_x=[100,100000]
+)
+
+fig.show()
