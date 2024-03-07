163b05a17ce031034b2088e51826ccdfb3e8e2a5
Kokchun <kokchungiang@gmail.com>
2024-03-07 09:40:08 +0100

    tested graph

--- code_alongs/Lec11_data_storytelling/story_charts.py
+++ code_alongs/Lec11_data_storytelling/story_charts.py
@@ -6,11 +6,29 @@ import pandas as pd
 # DATA_PATH = "../../data/data_processing/"
 DATA_PATH = Path(__file__).parents[2] / "data" / "data_processing"
 
+class StoryCharts:
+    def __init__(self) -> None:
+        pass
+
+    def _plot(self, x, y, colors = "#0c4a6e", **label_kwargs):
+        self.fig, self.ax = plt.subplots()
+
+        plt.show()
+    
+    def Line(self, x, y):
+        pass 
 
 # this is to be able to run this as a standalone script 
 # the code below won't run when imported from another script
 # as __name__ will be "story_charts.py" when imported
 if __name__ == "__main__":
     print("\n\n")
-    print(DATA_PATH)
-    print(__name__)
+    # print(DATA_PATH)
+    # print(__name__)
+
+    df = pd.read_csv(DATA_PATH / "co2_annmean_mlo.csv", skiprows=43)
+
+    print(df.head())
+
+    sc = StoryCharts()
+    sc._plot(2,3)
